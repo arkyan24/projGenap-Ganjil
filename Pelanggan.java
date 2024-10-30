5b11cde5d10cfbe0ff83452e0dda7b9fba89cae7
arkyan24 <aridhop24@gmail.com>
2024-10-30 07:13:33 +0700

    Pelanggan.java

--- Pelanggan.java
+++ Pelanggan.java
@@ -1,24 +1,26 @@
+import java.util.ArrayList;
 public class Pelanggan {
     private String nama;
     private String kontak;
-    private RiwayatServis riwayatServis; // Menyimpan hanya satu riwayat servis
+    private ArrayList<RiwayatServis> riwayatServis;
 
     public Pelanggan(String nama, String kontak) {
         this.nama = nama;
         this.kontak = kontak;
-        this.riwayatServis = null;
+        this.riwayatServis = new ArrayList<>();
     }
 
-    // Menambahkan atau memperbarui riwayat servis
+    // Menambahkan riwayat servis ke daftar
     public void setRiwayatServis(RiwayatServis servis) {
-        this.riwayatServis = servis;
+        this.riwayatServis.add(servis);
     }
-
-    // Menampilkan riwayat servis jika ada
+    
     public void tampilkanRiwayatServis() {
         System.out.println("Riwayat Servis untuk " + nama + ":");
-        if (riwayatServis != null) {
-            System.out.println("- " + riwayatServis.getLayanan() + " pada " + riwayatServis.getKendaraan().getInfoKendaraan());
+        if (!riwayatServis.isEmpty()) {
+            for (RiwayatServis servis : riwayatServis) {
+                System.out.println("- " + servis.getLayanan() + " pada " + servis.getKendaraan().getInfoKendaraan());
+            }
         } else {
             System.out.println("- Belum ada riwayat servis.");
         }
